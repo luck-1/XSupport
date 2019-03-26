651fea8226bb0346cd34e5a09c69a099a7735908
luck-1 <1986906062@qq.com>
2019-03-26 23:24:18 +0800

    11

---
+++ front/web/src/page/common/header.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="header">
+    <div class="icon">
+      <i class="icon iconfont icon-gerenxinxierweima"></i>
+    </div>
+    <div class="title">
+      <span>TITLE</span>
+    </div>
+
+    <div class="right">
+      <div class="user">user</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "common-header",
+  }
+</script>
+
+<style scoped>
+.header {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  font-size: 20px;
+  background-color: black;
+  color: aliceblue;
+  height: 60px;
+}
+</style>
---
+++ front/web/src/page/common/home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <vHeader />
+    </div>
+</template>
+
+<script>
+  import vHeader from './header'
+  export default {
+    name: "home",
+    components: {
+      vHeader
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
