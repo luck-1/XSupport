c71dcb29682fb5d60c0d7aed480679dac0b7caa0
luck-1 <1986906062@qq.com>
2019-07-16 14:34:53 +0800

    1

--- source/数据库/清库.sql
+++
@@ -1,8 +0,0 @@
-truncate TABLE displacement;
-truncate TABLE displacement_every;
-truncate TABLE displacement_value;
-truncate TABLE gas;
-truncate TABLE humidity;
-truncate TABLE soak;
-truncate TABLE sys_warn;
-truncate TABLE temperature;
