e3b93809eea957a55dab80626ddbfddb05e85e4a
luck-1 <1986906062@qq.com>
2019-07-24 18:29:33 +0800

    1

--- back/src/main/java/com/xsupport/model/manage/SysWarn.java
+++ back/src/main/java/com/xsupport/model/manage/SysWarn.java
@@ -27,7 +27,7 @@ public class SysWarn implements Serializable {
     @GeneratedValue(generator = "JDBC")
     @GenericGenerator(name = "JDBC", strategy = "uuid")
     @ApiModelProperty(value = "id")
-    @Column(name = "id",length = 32)
+    @Column(name = "id")
     private String id;
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
